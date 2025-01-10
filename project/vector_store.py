286a182e222ec38347df5397f3c5878de08bdb87
Kleryton Souza <kleryton.dev@gmail.com>
2025-01-10 14:23:27 -0300

    Adiciona carregamento e processamento de documentos PDF com vetorização e recuperação

--- project/vector_store.py
+++ project/vector_store.py
@@ -20,19 +20,23 @@ def initial_parameters() -> tuple:
 
 model, parser, client = initial_parameters() 
 
+# Carregamento do documento PDF
 pdf_path = 'data/laptop_manual.pdf'
 loader = PyPDFLoader(pdf_path)
-
 docs = loader.load()
 
+# Divisão do texto em pedaços
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=1000,
     chunk_overlap=200,
 )
+
+# Criação da cadeia de processamento para dividir o texto em pedaços
 chunks = text_splitter.split_documents(
     documents=docs,
 )
 
+# Criação do vetorizador
 embedding = OpenAIEmbeddings()
 vector_store = Chroma.from_documents(
     documents=chunks,
@@ -40,11 +44,13 @@ vector_store = Chroma.from_documents(
     collection_name='laptop_manual',
 )
 
+# Criação do recuperador
 retriever = vector_store.as_retriever()
 
 # Prompt para a IA se comportar como um assistente de pesquisa usando o RAG
 prompt = hub.pull('rlm/rag-prompt')
 
+# Cadeia de processamento para responder perguntas
 rag_chain = (
     {
         'context': retriever,
